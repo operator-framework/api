03c49b58ac430d271796cf1ac5b61fe7538afab6
dweepgogia <dweepgogia@gmail.com>
2019-08-07 17:37:13 -0400

    [cli] change from verify to manifest command
    - To avoid redundancy while using the cli (operator-verify manifest)
      instead of (operator-verify verify).

--- cmd/verify.go
+++ cmd/verify.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 
 	"github.com/dweepgogia/new-manifest-verification/pkg/validate"
-	"github.com/dweepgogia/new-manifest-verification/pkg/validate/validator"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +12,7 @@ func init() {
 }
 
 var verifyCmd = &cobra.Command{
-	Use:   "verify",
+	Use:   "manifest",
 	Short: "Validate YAML against OLM's CSV type.",
 	Long:  `Verifies the yaml file against Operator-Lifecycle-Manager's ClusterServiceVersion type. Reports errors for any mismatched data types. Takes in one argument i.e. path to the yaml file. Version: 1.0`,
 	Run:   verifyFunc,
@@ -26,10 +24,7 @@ func verifyFunc(cmd *cobra.Command, args []string) {
 		fmt.Printf("command %s requires exactly one argument", cmd.CommandPath())
 	}
 
-	yamlFileName := args[0]
+	manifestDirectory := args[0]
 
-	// TODO: return a pointer instead
-	if err := validate.ValidateCSVManifest(yamlFileName); err != (validator.Error{}) {
-		fmt.Println(err)
-	}
+	_ = validate.ValidateManifest(manifestDirectory)
 }
