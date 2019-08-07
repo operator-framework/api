968a8b4da3ee57a1195665c6fd586f51d5ac4167
dweepgogia <dweepgogia@gmail.com>
2019-08-07 17:44:33 -0400

    [lib] csv validator for example annotations
    - To compare example annotations to the provided APIs list
      and report appropriate errors.

--- pkg/validate/csv.go
+++ pkg/validate/csv.go
@@ -7,20 +7,21 @@ import (
 	"strings"
 
 	"github.com/dweepgogia/new-manifest-verification/pkg/validate/validator"
-	"github.com/pkg/errors"
-
-	olm "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	"github.com/ghodss/yaml"
+	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 type CSVValidator struct {
-	csvs []olm.ClusterServiceVersion
+	fileName string
+	csvs     []v1alpha1.ClusterServiceVersion
 }
 
 var _ validator.Validator = &CSVValidator{}
 
 func (v *CSVValidator) Validate() (results []validator.ManifestResult) {
 	for _, csv := range v.csvs {
-		// Contains error logs for all missing optional and mandatory fields.
 		result := csvInspect(csv)
 		if result.Name == "" {
 			result.Name = csv.GetName()
@@ -33,9 +34,9 @@ func (v *CSVValidator) Validate() (results []validator.ManifestResult) {
 func (v *CSVValidator) AddObjects(objs ...interface{}) validator.Error {
 	for _, o := range objs {
 		switch t := o.(type) {
-		case olm.ClusterServiceVersion:
+		case v1alpha1.ClusterServiceVersion:
 			v.csvs = append(v.csvs, t)
-		case *olm.ClusterServiceVersion:
+		case *v1alpha1.ClusterServiceVersion:
 			v.csvs = append(v.csvs, *t)
 		}
 	}
@@ -46,17 +47,41 @@ func (v CSVValidator) Name() string {
 	return "ClusterServiceVersion Validator"
 }
 
-// Iterates over the given CSV. Returns a ManifestResult type object.
-func csvInspect(val interface{}) validator.ManifestResult {
+func (v CSVValidator) FileName() string {
+	return v.fileName
+}
 
-	fieldValue := reflect.ValueOf(val)
+func (v CSVValidator) Unmarshal(rawYaml []byte) (interface{}, error) {
+	var csv v1alpha1.ClusterServiceVersion
+
+	rawJson, err := yaml.YAMLToJSON(rawYaml)
+	if err != nil {
+		return v1alpha1.ClusterServiceVersion{}, fmt.Errorf("error parsing raw YAML to Json: %s", err)
+	}
+	if err := json.Unmarshal(rawJson, &csv); err != nil {
+		return v1alpha1.ClusterServiceVersion{}, fmt.Errorf("error parsing CSV (JSON) : %s", err)
+	}
+	return csv, nil
+}
+
+// Iterates over the given CSV. Returns a ManifestResult type object.
+func csvInspect(csv v1alpha1.ClusterServiceVersion) validator.ManifestResult {
+
+	// validate example annotations ("alm-examples", "olm.examples").
+	manifestResult := validateExamplesAnnotations(csv)
+
+	// validate installModes
+	manifestResult = validateInstallModes(csv, manifestResult)
+
+	// check missing optional/mandatory fields.
+	fieldValue := reflect.ValueOf(csv)
 
 	switch fieldValue.Kind() {
 	case reflect.Struct:
-		return checkMissingFields(fieldValue, "", validator.ManifestResult{})
+		return checkMissingFields(fieldValue, "", manifestResult)
 	default:
 		errs := []validator.Error{
-			validator.InvalidCSV("Error: input file is not a valid CSV."),
+			validator.InvalidCSV("Error: input file is not a valid CSV"),
 		}
 
 		return validator.ManifestResult{Errors: errs, Warnings: nil}
@@ -90,13 +115,13 @@ func checkMissingFields(v reflect.Value, parentStructName string, log validator.
 
 		switch fieldValue.Kind() {
 		case reflect.Struct:
-			log = updateLog(log, "Struct", newParentStructName, emptyVal, isOptionalField)
+			log = updateLog(log, "struct", newParentStructName, emptyVal, isOptionalField)
 			if emptyVal {
 				continue
 			}
 			log = checkMissingFields(fieldValue, newParentStructName, log)
 		default:
-			log = updateLog(log, "Field", newParentStructName, emptyVal, isOptionalField)
+			log = updateLog(log, "field", newParentStructName, emptyVal, isOptionalField)
 		}
 	}
 	return log
@@ -106,14 +131,12 @@ func checkMissingFields(v reflect.Value, parentStructName string, log validator.
 func updateLog(log validator.ManifestResult, typeName string, newParentStructName string, emptyVal bool, isOptionalField bool) validator.ManifestResult {
 
 	if emptyVal && isOptionalField {
-		err := errors.Errorf("Warning: Optional %s Missing", typeName)
 		// TODO: update the value field (typeName).
-		log.Warnings = append(log.Warnings, validator.OptionalFieldMissing(newParentStructName, typeName, err.Error()))
+		log.Warnings = append(log.Warnings, validator.OptionalFieldMissing(fmt.Sprintf("Warning: optional %s missing: (%s)", typeName, newParentStructName), newParentStructName, typeName))
 	} else if emptyVal && !isOptionalField {
 		if newParentStructName != "Status" {
-			err := errors.Errorf("Error: Mandatory %s Missing", typeName)
 			// TODO: update the value field (typeName).
-			log.Errors = append(log.Errors, validator.MandatoryFieldMissing(newParentStructName, typeName, err.Error()))
+			log.Errors = append(log.Errors, validator.MandatoryFieldMissing(fmt.Sprintf("Error: mandatory %s missing: (%s)", typeName, newParentStructName), newParentStructName, typeName))
 		}
 	}
 	return log
@@ -162,4 +185,129 @@ func isEmptyValue(v reflect.Value) bool {
 	default:
 		panic(fmt.Sprintf("%v kind is not supported.", v.Kind()))
 	}
+}
+
+// validateExamplesAnnotations compares alm/olm example annotations with provided APIs given
+// by Spec.CustomResourceDefinitions.Owned and Spec.APIServiceDefinitions.Owned.
+func validateExamplesAnnotations(csv v1alpha1.ClusterServiceVersion) (manifestResult validator.ManifestResult) {
+	var examples []v1beta1.CustomResourceDefinition
+	var annotationsExamples string
+	annotations := csv.ObjectMeta.GetAnnotations()
+	// Return right away if no examples annotations are found.
+	if len(annotations) == 0 {
+		manifestResult.Warnings = append(manifestResult.Warnings, validator.InvalidCSV(fmt.Sprintf("Warning: example annotations not found for %s csv", csv.GetName())))
+		return
+	}
+	// Expect either `alm-examples` or `olm.examples` but not both
+	// If both are present, `alm-examples` will be used
+	if value, ok := annotations["alm-examples"]; ok {
+		annotationsExamples = value
+		if _, ok = annotations["olm.examples"]; ok {
+			// both `alm-examples` and `olm.examples` are present
+			manifestResult.Warnings = append(manifestResult.Warnings, validator.InvalidCSV(fmt.Sprintf("Warning: both `alm-examples` and `olm.examples` are present in %s CSV. Defaulting to `alm-examples` and ignoring `olm.examples`", csv.GetName())))
+		}
+	} else {
+		annotationsExamples = annotations["olm.examples"]
+	}
+
+	// Can't find examples annotations, simply return
+	if annotationsExamples == "" {
+		manifestResult.Warnings = append(manifestResult.Warnings, validator.InvalidCSV(fmt.Sprintf("Warning: example annotations not found for %s csv", csv.GetName())))
+		return
+	}
+
+	if err := json.Unmarshal([]byte(annotationsExamples), &examples); err != nil {
+		manifestResult = getManifestResult(validator.InvalidParse(fmt.Sprintf("Error: parsing example annotations to %T type:  %s ", examples, err), nil))
+		return
+	}
+
+	providedAPIs, manRes := getProvidedAPIs(csv, manifestResult)
+
+	parsedExamples, manRes := parseExamplesAnnotations(examples, manifestResult)
+	if len(manRes.Errors) != 0 || len(manRes.Warnings) != 0 {
+		return manRes
+	}
+
+	return matchGVKProvidedAPIs(parsedExamples, providedAPIs, manifestResult)
+}
+
+func getProvidedAPIs(csv v1alpha1.ClusterServiceVersion, manifestResult validator.ManifestResult) (map[schema.GroupVersionKind]struct{}, validator.ManifestResult) {
+	provided := map[schema.GroupVersionKind]struct{}{}
+
+	for _, owned := range csv.Spec.CustomResourceDefinitions.Owned {
+		parts := strings.SplitN(owned.Name, ".", 2)
+		if len(parts) < 2 {
+			manifestResult.Errors = append(manifestResult.Errors, validator.InvalidParse(fmt.Sprintf("Error: couldn't parse plural.group from crd name: %s", owned.Name), owned.Name))
+			continue
+		}
+		provided[schema.GroupVersionKind{Group: parts[1], Version: owned.Version, Kind: owned.Kind}] = struct{}{}
+	}
+
+	for _, api := range csv.Spec.APIServiceDefinitions.Owned {
+		provided[schema.GroupVersionKind{Group: api.Group, Version: api.Version, Kind: api.Kind}] = struct{}{}
+	}
+
+	return provided, manifestResult
+}
+
+func parseExamplesAnnotations(examples []v1beta1.CustomResourceDefinition, manifestResult validator.ManifestResult) (map[schema.GroupVersionKind]struct{}, validator.ManifestResult) {
+	parsed := map[schema.GroupVersionKind]struct{}{}
+	for _, value := range examples {
+		parts := strings.SplitN(value.APIVersion, "/", 2)
+		if len(parts) < 2 {
+			manifestResult.Errors = append(manifestResult.Errors, validator.InvalidParse(fmt.Sprintf("Error: couldn't parse group/version from crd kind: %s", value.Kind), value.Kind))
+			continue
+		}
+		parsed[schema.GroupVersionKind{Group: parts[0], Version: parts[1], Kind: value.Kind}] = struct{}{}
+	}
+
+	return parsed, manifestResult
+}
+
+func matchGVKProvidedAPIs(examples map[schema.GroupVersionKind]struct{}, providedAPIs map[schema.GroupVersionKind]struct{}, manifestResult validator.ManifestResult) validator.ManifestResult {
+	for key := range examples {
+		if _, ok := providedAPIs[key]; !ok {
+			manifestResult.Errors = append(manifestResult.Errors, validator.InvalidOperation(fmt.Sprintf("Error: couldn't match %v in provided APIs list: %v", key, providedAPIs), key))
+			continue
+		}
+	}
+	return manifestResult
+}
+
+func getManifestResult(errs ...validator.Error) validator.ManifestResult {
+	errList := append([]validator.Error{}, errs...)
+	return validator.ManifestResult{Errors: errList, Warnings: nil}
+}
+
+func validateInstallModes(csv v1alpha1.ClusterServiceVersion, manifestResult validator.ManifestResult) validator.ManifestResult {
+	// var installModeSet v1alpha1.InstallModeSet
+	installModeSet := make(v1alpha1.InstallModeSet)
+	for _, installMode := range csv.Spec.InstallModes {
+		if _, ok := installModeSet[installMode.Type]; ok {
+			manifestResult.Errors = append(manifestResult.Errors, validator.InvalidCSV(fmt.Sprintf("Error: duplicate install modes present in %s csv", csv.GetName())))
+		} else {
+			installModeSet[installMode.Type] = installMode.Supported
+		}
+	}
+
+	// installModes not found, return with a warning
+	if len(installModeSet) == 0 {
+		manifestResult.Warnings = append(manifestResult.Warnings, validator.InvalidCSV(fmt.Sprintf("Warning: install modes not found for %s csv", csv.GetName())))
+		return manifestResult
+	}
+
+	// all installModes should not be `false`
+	if checkAllFalseForInstallModeSet(installModeSet) {
+		manifestResult.Errors = append(manifestResult.Errors, validator.InvalidCSV(fmt.Sprintf("Error: none of InstallModeTypes are supported for %s csv", csv.GetName())))
+	}
+	return manifestResult
+}
+
+func checkAllFalseForInstallModeSet(installModeSet v1alpha1.InstallModeSet) bool {
+	for _, isSupported := range installModeSet {
+		if isSupported {
+			return false
+		}
+	}
+	return true
 }
