96f3edbb12a4369ea15984a54da78af54f53c9ce
njhale <njohnhale@gmail.com>
2019-09-24 17:20:53 -0400

    feat(operators): add operator type to v2alpha1

---
+++ operators/register.go
@@ -0,0 +1,6 @@
+package operators
+
+const (
+	// GroupName is the group name used in this package
+	GroupName = "operators.coreos.com"
+)
---
+++ operators/v2alpha1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +groupName=operators.coreos.com
+
+// Package v2alpha1 is the v2alpha1 version of the API.
+package v2alpha1 // import "github.com/operator-framework/api/operators/v2alpha1"
