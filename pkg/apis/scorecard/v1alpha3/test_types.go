914e2f7a8919f5fd0d2d91a02b1cbb93d3327dff
Ish Shah <ishah@redhat.com>
2021-07-27 08:26:32 -0700

    Update pkg/apis/scorecard/v1alpha3/test_types.go
    Co-authored-by: Joe Lanford <joe.lanford@gmail.com>

--- pkg/apis/scorecard/v1alpha3/test_types.go
+++ pkg/apis/scorecard/v1alpha3/test_types.go
@@ -28,8 +28,8 @@ type TestResult struct {
 	Errors []string `json:"errors,omitempty"`
 	// Suggestions is a list of suggestions for the user to improve their score (if applicable)
 	Suggestions []string `json:"suggestions,omitempty"`
-	// Timestamp of the result from an invidual test
-	Tstamp metav1.Time `json:"tstamp,omitempty"`
+	// CreationTimestamp of the result from an invidual test
+	CreationTimestamp metav1.Time `json:"creationTimestamp,omitempty"`
 }
 
 // TestStatus contains collection of testResults.
