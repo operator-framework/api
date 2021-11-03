d8ed477399677d3729b11eaff77a5d9a831bc2e3
Anik Bhattacharjee <anikbhattacharya93@gmail.com>
2021-11-03 17:08:25 -0400

    (catsrc) add custom unmarshaller for registry poll interval
    This PR implements a custom unmarshaller for the CatalogSourceSpec.UpdateStrategy.
    When a value that cannot be unmarshalled to *metav1.Duration was being passed to
    UpdateStrategy.RegistryPoll.Interval, the catalogSource sync loops in the catalog
    operator was stuck in an infinite loop instead of backing off and performing other
    syncs. The custom unmarshaller tries to unmarshall the input to the field to
    *metav1.Duration, and if it fails, it sets a default value of 15m for the field.
    Otherwise it accepts the value passed to the field.

--- pkg/operators/v1alpha1/types_test.go
+++ pkg/operators/v1alpha1/types_test.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	"encoding/json"
+	"fmt"
 	"sort"
 	"testing"
 	"time"
@@ -206,5 +208,74 @@ func TestCatalogSource_Poll(t *testing.T) {
 	}
 	for i, tt := range table {
 		require.Equal(t, tt.result, table[i].catsrc.Poll(), table[i].description)
+	}
+}
+
+func TestUpdateStrategyUnmarshal(t *testing.T) {
+	type TestStruct struct {
+		UpdateStrategy UpdateStrategy `json:"updateStrategy,omitempty"`
+	}
+	validDuration, err := time.ParseDuration("45m")
+	if err != nil {
+		panic(fmt.Errorf("error parsing duration: %s", err))
+	}
+	defaultDuration, err := time.ParseDuration("15m")
+	if err != nil {
+		panic(fmt.Errorf("error parsing duration: %s", err))
+	}
+	tests := []struct {
+		name string
+		in   []byte
+		out  TestStruct
+		err  error
+	}{
+		{
+			name: "valid",
+			in:   []byte(`{"UpdateStrategy": {"registryPoll":{"interval":"45m"}}}`),
+			out: TestStruct{
+				UpdateStrategy{
+					&RegistryPoll{
+						RawInterval:  "45m",
+						Interval:     &metav1.Duration{Duration: validDuration},
+						ParsingError: "",
+					},
+				},
+			},
+		},
+		{
+			name: "invalid",
+			in:   []byte(`{"UpdateStrategy": {"registryPoll":{"interval":"19mError Code"}}}`),
+			out: TestStruct{
+				UpdateStrategy{
+					&RegistryPoll{
+						RawInterval:  "19mError Code",
+						Interval:     &metav1.Duration{Duration: defaultDuration},
+						ParsingError: "error parsing spec.updateStrategy.registryPoll.interval. Using the default value of 15m0s instead. Error: time: unknown unit \"mError Code\" in duration \"19mError Code\"",
+					},
+				},
+			},
+		},
+		{
+			name: "empty",
+			in:   []byte(`{"UpdateStrategy": {"registryPoll":{"interval":""}}}`),
+			out: TestStruct{
+				UpdateStrategy{
+					&RegistryPoll{
+						Interval:     &metav1.Duration{Duration: defaultDuration},
+						ParsingError: "error parsing spec.updateStrategy.registryPoll.interval. Using the default value of 15m0s instead. Error: time: invalid duration \"\"",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := TestStruct{}
+			err := json.Unmarshal(tt.in, &s)
+			require.Equal(t, tt.out.UpdateStrategy.RawInterval, s.UpdateStrategy.RawInterval)
+			require.Equal(t, tt.out.UpdateStrategy.Interval, s.UpdateStrategy.Interval)
+			require.Equal(t, tt.out.UpdateStrategy.ParsingError, s.UpdateStrategy.ParsingError)
+			require.Equal(t, tt.err, err)
+		})
 	}
 }
