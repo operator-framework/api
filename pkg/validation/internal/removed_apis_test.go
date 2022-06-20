0209b8b5e1f926bbca25929b34fe7d422f403dbc
Per Goncalves da Silva <perdasilva@redhat.com>
2022-06-20 08:56:55 +0200

    fix removed_apis_test (#244)
    Signed-off-by: perdasilva <perdasilva@redhat.com>

--- pkg/validation/internal/removed_apis_test.go
+++ pkg/validation/internal/removed_apis_test.go
@@ -1,10 +1,11 @@
 package internal
 
 import (
-	"github.com/operator-framework/api/pkg/manifests"
-	"github.com/stretchr/testify/require"
 	"reflect"
 	"testing"
+
+	"github.com/operator-framework/api/pkg/manifests"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_GetRemovedAPIsOn1_22From(t *testing.T) {
