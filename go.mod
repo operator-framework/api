5c7125323f052935741915349ad5c1d02eeb8bf7
Jordan Keister <jordan@nimblewidget.com>
2022-10-05 10:24:08 -0500

    attempt to publish FBC read/write updated all paths for fork fix invalid internal error redirecting for merge from test-stability retaining alpha status of constituent declcfg pieces move out of pkg/lib merge declcfg-->fbc from operator-registry/249ae621 forgot to repackage during path change fixing generated whitespace-only changes
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>

--- go.mod
+++ go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-bindata/go-bindata/v3 v3.1.3
 	github.com/google/cel-go v0.12.4
+	github.com/h2non/filetype v1.1.3
+	github.com/h2non/go-is-svg v0.0.0-20160927212452-35e8c4b0612c
+	github.com/joelanford/ignore v0.0.0-20210610194209-63d4919d8fb2
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.0
@@ -16,6 +19,7 @@ require (
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
 	sigs.k8s.io/controller-runtime v0.13.0
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
@@ -27,6 +31,9 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.1.0 // indirect
+	github.com/go-git/go-git/v5 v5.3.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
@@ -40,6 +47,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kisielk/errcheck v1.5.0 // indirect
@@ -78,6 +86,7 @@ require (
 	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiserver v0.25.0 // indirect
@@ -88,5 +97,4 @@ require (
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.0.32 // indirect
 	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
---
+++ pkg/alpha/fbc/load.go
@@ -0,0 +1,233 @@
+package fbc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"path/filepath"
+	"strings"
+
+	"github.com/joelanford/ignore"
+	"github.com/operator-framework/api/pkg/operators"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/yaml"
+
+	"github.com/operator-framework/api/pkg/alpha/property"
+)
+
+const (
+	indexIgnoreFilename = ".indexignore"
+)
+
+type WalkFunc func(path string, cfg *DeclarativeConfig, err error) error
+
+// WalkFS walks root using a gitignore-style filename matcher to skip files
+// that match patterns found in .indexignore files found throughout the filesystem.
+// It calls walkFn for each declarative config file it finds. If WalkFS encounters
+// an error loading or parsing any file, the error will be immediately returned.
+func WalkFS(root fs.FS, walkFn WalkFunc) error {
+	if root == nil {
+		return fmt.Errorf("no declarative config filesystem provided")
+	}
+
+	matcher, err := ignore.NewMatcher(root, indexIgnoreFilename)
+	if err != nil {
+		return err
+	}
+
+	return fs.WalkDir(root, ".", func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return walkFn(path, nil, err)
+		}
+		// avoid validating a directory, an .indexignore file, or any file that matches
+		// an ignore pattern outlined in a .indexignore file.
+		if info.IsDir() || info.Name() == indexIgnoreFilename || matcher.Match(path, false) {
+			return nil
+		}
+
+		cfg, err := LoadFile(root, path)
+		if err != nil {
+			return walkFn(path, cfg, err)
+		}
+
+		return walkFn(path, cfg, err)
+	})
+}
+
+// LoadFS loads a declarative config from the provided root FS. LoadFS walks the
+// filesystem from root and uses a gitignore-style filename matcher to skip files
+// that match patterns found in .indexignore files found throughout the filesystem.
+// If LoadFS encounters an error loading or parsing any file, the error will be
+// immediately returned.
+func LoadFS(root fs.FS) (*DeclarativeConfig, error) {
+	cfg := &DeclarativeConfig{}
+	if err := WalkFS(root, func(path string, fcfg *DeclarativeConfig, err error) error {
+		if err != nil {
+			return err
+		}
+		cfg.Packages = append(cfg.Packages, fcfg.Packages...)
+		cfg.Channels = append(cfg.Channels, fcfg.Channels...)
+		cfg.Bundles = append(cfg.Bundles, fcfg.Bundles...)
+		cfg.Others = append(cfg.Others, fcfg.Others...)
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func readBundleObjects(bundles []Bundle, root fs.FS, path string) error {
+	for bi, b := range bundles {
+		props, err := property.Parse(b.Properties)
+		if err != nil {
+			return fmt.Errorf("package %q, bundle %q: parse properties: %v", b.Package, b.Name, err)
+		}
+		for oi, obj := range props.BundleObjects {
+			objID := fmt.Sprintf(" %q", obj.GetRef())
+			if !obj.IsRef() {
+				objID = fmt.Sprintf("[%d]", oi)
+			}
+
+			d, err := obj.GetData(root, filepath.Dir(path))
+			if err != nil {
+				return fmt.Errorf("package %q, bundle %q: get data for bundle object%s: %v", b.Package, b.Name, objID, err)
+			}
+			objJson, err := yaml.ToJSON(d)
+			if err != nil {
+				return fmt.Errorf("package %q, bundle %q: convert object%s to JSON: %v", b.Package, b.Name, objID, err)
+			}
+			bundles[bi].Objects = append(bundles[bi].Objects, string(objJson))
+		}
+		bundles[bi].CsvJSON = extractCSV(bundles[bi].Objects)
+	}
+	return nil
+}
+
+func extractCSV(objs []string) string {
+	for _, obj := range objs {
+		u := unstructured.Unstructured{}
+		if err := yaml.Unmarshal([]byte(obj), &u); err != nil {
+			continue
+		}
+		if u.GetKind() == operators.ClusterServiceVersionKind {
+			return obj
+		}
+	}
+	return ""
+}
+
+// LoadReader reads yaml or json from the passed in io.Reader and unmarshals it into a DeclarativeConfig struct.
+// Path references will not be de-referenced so callers are responsible for de-referencing if necessary.
+func LoadReader(r io.Reader) (*DeclarativeConfig, error) {
+	cfg := &DeclarativeConfig{}
+	dec := yaml.NewYAMLOrJSONDecoder(r, 4096)
+	for {
+		doc := json.RawMessage{}
+		if err := dec.Decode(&doc); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, err
+		}
+		doc = []byte(strings.NewReplacer(`\u003c`, "<", `\u003e`, ">", `\u0026`, "&").Replace(string(doc)))
+
+		var in Meta
+		if err := json.Unmarshal(doc, &in); err != nil {
+			return nil, fmt.Errorf("unmarshal error: %s", resolveUnmarshalErr(doc, err))
+		}
+
+		switch in.Schema {
+		case SchemaPackage:
+			var p Package
+			if err := json.Unmarshal(doc, &p); err != nil {
+				return nil, fmt.Errorf("parse package: %v", err)
+			}
+			cfg.Packages = append(cfg.Packages, p)
+		case SchemaChannel:
+			var c Channel
+			if err := json.Unmarshal(doc, &c); err != nil {
+				return nil, fmt.Errorf("parse channel: %v", err)
+			}
+			cfg.Channels = append(cfg.Channels, c)
+		case SchemaBundle:
+			var b Bundle
+			if err := json.Unmarshal(doc, &b); err != nil {
+				return nil, fmt.Errorf("parse bundle: %v", err)
+			}
+			cfg.Bundles = append(cfg.Bundles, b)
+		case "":
+			return nil, fmt.Errorf("object '%s' is missing root schema field", string(doc))
+		default:
+			cfg.Others = append(cfg.Others, in)
+		}
+	}
+	return cfg, nil
+}
+
+// LoadFile will unmarshall declarative config components from a single filename provided in 'path'
+// located at a filesystem hierarchy 'root'
+func LoadFile(root fs.FS, path string) (*DeclarativeConfig, error) {
+	file, err := root.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	cfg, err := LoadReader(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := readBundleObjects(cfg.Bundles, root, path); err != nil {
+		return nil, fmt.Errorf("read bundle objects: %v", err)
+	}
+
+	return cfg, nil
+}
+
+func resolveUnmarshalErr(data []byte, err error) string {
+	var te *json.UnmarshalTypeError
+	if errors.As(err, &te) {
+		return formatUnmarshallErrorString(data, te.Error(), te.Offset)
+	}
+	var se *json.SyntaxError
+	if errors.As(err, &se) {
+		return formatUnmarshallErrorString(data, se.Error(), se.Offset)
+	}
+	return err.Error()
+}
+
+func formatUnmarshallErrorString(data []byte, errmsg string, offset int64) string {
+	sb := new(strings.Builder)
+	_, _ = sb.WriteString(fmt.Sprintf("%s at offset %d (indicated by <==)\n ", errmsg, offset))
+	// attempt to present the erroneous JSON in indented, human-readable format
+	// errors result in presenting the original, unformatted output
+	var pretty bytes.Buffer
+	err := json.Indent(&pretty, data, "", "    ")
+	if err == nil {
+		pString := pretty.String()
+		// calc the prettified string offset which correlates to the original string offset
+		var pOffset, origOffset int64
+		origOffset = 0
+		for origOffset = 0; origOffset < offset; {
+			pOffset++
+			if pString[pOffset] != '\n' && pString[pOffset] != ' ' {
+				origOffset++
+			}
+		}
+		_, _ = sb.WriteString(pString[:pOffset])
+		_, _ = sb.WriteString(" <== ")
+		_, _ = sb.WriteString(pString[pOffset:])
+	} else {
+		for i := int64(0); i < offset; i++ {
+			_ = sb.WriteByte(data[i])
+		}
+		_, _ = sb.WriteString(" <== ")
+		_, _ = sb.Write(data[offset:])
+	}
+
+	return sb.String()
+}
